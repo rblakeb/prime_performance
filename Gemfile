f45a86aeb2d874e4da5956708b35c3dc66ae8bb8
Blake Barnes <blabar3@gmail.com>
2018-08-05 23:16:31 -0500

    Added Wodify form to contact page//completed contact page//installed square-connect gem.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
 gem "font-awesome-rails"
+gem 'square_connect', '~> 2.20180712', '>= 2.20180712.1.234'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,21 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+function validateWodifyLeadForm()
+{
+	var reEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	var returnValue = true;var fieldname=document.forms['wodifyLeadForm']['name'].value;
+        if(fieldname==null||fieldname=='')
+            {document.getElementById('errorname').style.display='inline';returnValue=false;}
+        else
+            {document.getElementById('errorname').style.display='none';}var fieldemail=document.forms['wodifyLeadForm']['email'].value;
+	    if(fieldemail==null||fieldemail==''||!reEmail.test(fieldemail))
+	        {document.getElementById('erroremail').style.display='inline';returnValue=false;}
+	    else
+	        {document.getElementById('erroremail').style.display='none';}var fieldlocation=document.forms['wodifyLeadForm']['location'].options[document.forms['wodifyLeadForm']['location'].selectedIndex].value;if(fieldlocation==null||fieldlocation=='')
+	{document.getElementById('errorlocation').style.display='inline';returnValue=false;}
+		else
+	{document.getElementById('errorlocation').style.display='none';} return returnValue;
+}; 
