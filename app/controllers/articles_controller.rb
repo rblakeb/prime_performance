67b725fc402a4617092512df1870ab099c2d1e9e
Blake Barnes <blabar3@gmail.com>
2018-10-30 08:55:04 -0500

    forgot to change branches so i used this one to create articles model for seo purposes.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :delete]
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully published'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to article_path, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def delete
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+
+    respond_to do |format|
+      format.html { redirect_to articles_path, notice: 'Article successfully deleted' }
+      format.json { head :ok } 
+      format.js
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -9,13 +9,17 @@
 
 # SitemapGenerator::Sitemap.public_path = 'tmp/'
 # SitemapGenerator::Sitemap.sitemaps_host = "https://s3.console.aws.amazon.com/s3/buckets/owlc/sitemaps/?region=us-west-2&tab=overview"
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+# SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.primeperformancesc.com"
+# SitemapGenerator::Sitemap.default_host = "https://www.primeperformancesc.com"
 
-SitemapGenerator::Sitemap.create do
+SitemapGenerator::Sitemap.create(
+  default_host: "https://www.primeperformancesc.com",
+  sitemaps_path: 'sitemaps/',
+  ) do
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
@@ -35,22 +39,23 @@ SitemapGenerator::Sitemap.create do
   #
   # Add all articles:
   #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 
-  add blogs_path, :priority => 0.5, :changefreq => 'daily'
-  Blog.find_each do |b|
-    add blog_path(b), :lastmod => b.updated_at, :priority => 0.5
-  end
+    add blogs_path, :priority => 0.5, :changefreq => 'daily'
+    Blog.find_each do |b|
+      add blog_path(b), :lastmod => b.updated_at, :priority => 0.5
+    end
 
-  add workouts_path, :priority => 0.5, :changefreq => 'daily'
-  Workout.find_each do |w|
-    add workout_path(w), :lastmod => w.updated_at, :priority => 0.5, :changefreq => 'daily'
-  end
+    add workouts_path, :priority => 0.5, :changefreq => 'daily'
+    Workout.find_each do |w|
+      add workout_path(w), :lastmod => w.updated_at, :priority => 0.5, :changefreq => 'daily'
+    end
 
-  add coaches_path :priority => 0.5
-  add contact_path :priority => 0.5
-  add packages_path :priority => 0.5
+    add coaches_path :priority => 0.5
+    add contact_path :priority => 0.5
+    add packages_path :priority => 0.5
+
+    Article.find_each do |article|
+      add article_path(article), :lastmod => article.updated_at
+    end
 
 end
