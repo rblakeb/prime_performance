863e6f92654247cf11492614ffa65d893deef1c5
Blake Barnes <blabar3@gmail.com>
2019-03-13 14:19:50 -0500

    added some shit

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module PrimePerformance
 
     config.time_zone = "Central Time (US & Canada)"
 
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # For Heroku to work with Ckeditor
     config.assets.precompile += Ckeditor.assets
